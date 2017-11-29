73828ef067dcd804c71717af063edfd93d7d389f
Stephan Oudmaijer <soudmaijer@gmail.com>
2017-11-29 23:43:13 +0100

    Working exercise 2

---
+++ src/main/java/com/bootique/bootique/OrderItem.kt
@@ -0,0 +1,7 @@
+package com.bootique.bootique
+
+import java.math.BigDecimal
+
+data class OrderItem(val productId: String, val quantity: Int, val price: BigDecimal = BigDecimal.ZERO) {
+    val totalPrice: BigDecimal = price * BigDecimal(quantity)
+}
