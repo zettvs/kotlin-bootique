1d491bf3553aafda7a2b45312129d2d7bf740c04
Stephan Oudmaijer <soudmaijer@gmail.com>
2017-11-30 08:40:25 +0100

    polish

--- src/main/java/com/bootique/bootique/OrderItem.java
+++ src/main/java/com/bootique/bootique/OrderItem.java
@@ -18,7 +18,6 @@ public class OrderItem {
         this.price = price;
     }
 
-
     public OrderItem(String productId, int quantity, BigDecimal price) {
         this.productId = productId;
         this.quantity = quantity;
@@ -39,5 +38,34 @@ public class OrderItem {
 
     public BigDecimal getTotalPrice() {
         return price.multiply(new BigDecimal(quantity));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OrderItem orderItem = (OrderItem) o;
+
+        if (quantity != orderItem.quantity) return false;
+        if (productId != null ? !productId.equals(orderItem.productId) : orderItem.productId != null) return false;
+        return price != null ? price.equals(orderItem.price) : orderItem.price == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = productId != null ? productId.hashCode() : 0;
+        result = 31 * result + quantity;
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItem{" +
+                "productId='" + productId + '\'' +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                '}';
     }
 }
